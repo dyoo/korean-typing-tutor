e1f981f427931507f93d9af911ad2d2f396f701f
Danny Yoo <danny.yoo@gmail.com>
2026-08-15 14:30:23 -0700

    Add some suppressions for a11y; it's a typing tutor, so a keyboard is necessary

--- src/lib/CharDisplay.svelte
+++ src/lib/CharDisplay.svelte
@@ -45,6 +45,8 @@
   }
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <span
   bind:this={elementRef}
   data-target-index={dataIndex}
